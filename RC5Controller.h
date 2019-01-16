3b13fab97dacf6fc162f111aa87ae03fc6e67dd0
Gergo Huszty <gergo.huszty@digitaltrip.hu>
2019-01-16 19:44:13 +0100

    RC5Controller got leaning trigger interface

--- RC5Controller.h
+++ RC5Controller.h
@@ -12,8 +12,14 @@
 #include "RC5.h"
 
 class RC5CommandHandler {
-public:
+  public:
   virtual void buttonPressed() = 0;
+};
+
+class RC5LearningHook {
+  public:
+  virtual void startLearningOf(uint8_t index) = 0;
+  virtual void doneLearningOf(uint8_t index, uint16_t command) = 0;
 };
 
 template<uint8_t T_SIZE> class RC5Controller: public RC5Handler {
@@ -21,23 +27,39 @@ template<uint8_t T_SIZE> class RC5Controller: public RC5Handler {
 	RC5Controller& operator=( const RC5Controller &c );
   RC5CommandHandler *handlers[T_SIZE];
   uint16_t commands[T_SIZE];
+  RC5LearningHook *hook;
+  int8_t learningIndex;
 public:
-	RC5Controller() {};
+	RC5Controller(RC5LearningHook *newHook): hook(newHook), learningIndex(-1) {};
 	~RC5Controller() {};
   void commandReceived(uint16_t message) {
     message &= ~(TOGGLE_MASK);
-    uint8_t i = 0;
-    do {
-      if(message == commands[i] && handlers[i]) {
-        handlers[i]->buttonPressed();
+    if(learningIndex >=0) {
+      hook->doneLearningOf(learningIndex, message);
+      setCommand(learningIndex, message);
+      if(++learningIndex < T_SIZE) {
+        hook->startLearningOf(learningIndex);
+      } else {        
+        learningIndex = -1;
       }
-    } while(++i < T_SIZE);
+    } else {
+      uint8_t i = 0;
+      do {
+        if(message == commands[i] && handlers[i]) {
+          handlers[i]->buttonPressed();
+        }
+      } while(++i < T_SIZE);
+    }    
   }
   void setHandler(uint8_t index, RC5CommandHandler *handler) {
     handlers[index] = handler;
   }
   void setCommand(uint8_t index, uint16_t command) {
     commands[index] = command;
+  }
+  void startLearningAll() {
+    learningIndex = 0;
+    hook->startLearningOf(0);
   }
 };
 
